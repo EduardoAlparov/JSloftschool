954a267d4c4f7bec805b49d89b1f6dc82c43cfb8
Eduard Alparov <workman133@yandex.ru>
2020-10-28 15:41:35 +0300

    первый коммит гео-отзыв

---
+++ projects/geo-review/interactiveMap.js
@@ -0,0 +1,75 @@
+/* global ymaps*/
+
+export default class InteractiveMap {
+  constructor(mapId, onClick) {
+    this.mapId = mapId;
+    this.onClick = onClick;
+  }
+
+  async init() {
+    await this.injectYMapsScript();
+    await this.loadMaps();
+    this.initMap();
+  }
+
+  injectYMapsScript() {
+    return new Promise((resolve) => {
+      const ymapsScript = document.createElement('script');
+      ymapsScript.src =
+        'https://api-maps.yandex.ru/2.1/?apikey=d02525f1-2a0d-4700-a5e1-e4487f06702c&lang=ru_RU';
+      document.body.appendChild(ymapsScript);
+      ymapsScript.addEventListener('load', resolve);
+    });
+  }
+
+  loadMaps() {
+    return new Promise((resolve) => ymaps.ready(resolve));
+  }
+
+  initMap() {
+    this.clusterer = new ymaps.Clusterer({
+      groupByCoordinetes: true,
+      clusterDisableClickZoom: true,
+      clusterOpenBalloonOnClick: false,
+    });
+    this.clusterer.events.add('click', (e) => {
+      const coords = e.get('target').geometry.getCoordinates();
+      this.onClick(coords);
+    });
+    this.map = new ymaps.Map(this.mapId, {
+      center: [55.77, 49.15],
+      zoom: 14,
+      controls: [],
+    });
+    this.map.controls.add('zoomControl');
+    this.map.events.add('click', (e) => this.onClick(e.get('coords')));
+    this.map.geoObjects.add(this.clusterer);
+  }
+
+  openBalloon(coords, content) {
+    this.map.balloon.open(coords, content);
+  }
+
+  setBalloonContent(content) {
+    this.map.balloon.setData(content);
+  }
+
+  closeBalloon() {
+    this.map.balloon.close();
+  }
+
+  createPlacemark(coords) {
+    const placemark = new ymaps.Placemark(coords, {
+      iconLayout: 'default#image',
+      iconImageHref: require('./img/pin.png'),
+      iconImageSize: [60, 84],
+      iconImageOffset: [-30, -42],
+    });
+
+    placemark.events.add('click', (e) => {
+      const coords = e.get('target').geometry.getCoordinates();
+      this.onClick(coords);
+    });
+    this.clusterer.add(placemark);
+  }
+}
